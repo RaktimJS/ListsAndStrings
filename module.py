c279a207a7ae1e3e94addb5853f1a5ec0f5290da
RaktimJS <raktimunreal4@gmail.com>
2024-12-16 14:50:32 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,448 @@
+"""
+
+TURN LINE WRAPPING OFF FOR A BETTER VIEW
+
++-------------------------------------------------------------------+
+|***------------   MODULE0   ------------***                        |
+|                                                                   |
+|                                                                   |
+|Author: Raktim JS                                                  |
+|Institution: Pathsala Public School, Pathsala, Seuj Nagar, 781325  |
+|Date: 15/12/2024                                                   |
+|Email: raktimunreal4@gmail.com                                     |
+|                                                                   |
+|                                                                   |
+|                        DO NOT PLAGIARISE                          |
+|                                                                   |
+|                                                                   |
+|Includes functions to:                                             |
+|        1. Remove all occurrences of a particular character from a |
+|           given string                                            |
+|        2. Check the no. of instances of a particular character in |
+|           a string                                                |
+|        3. Take a list of integers and create two different lists  |
+|           of positive integers and negative integers              |
+|        4. Merge two lists without duplicate values                |
+|        5. Remove all duplicates in a given list and return another|
+|           list of only duplicates                                 |
+|        6. CHECK module1.py                                        |
++-------------------------------------------------------------------+
+
+Last Edited on 16/12/2024
+
+"""
+
+
+
+
+from random import randint
+
+elements = [
+        'Algorithm', 'Compiler', 'Debug', 'Firmware', 'Cloud',
+        'Cache', 'Kernel', 'Pixel', 'Encryption', 'Bandwidth',
+        'Circuit', 'Server', 'Protocol', 'GPU', 'Database',
+        'Blockchain', 'Syntax', 'Java', 'Python', 'API',
+        'Bitrate', 'Quantum', 'Emulator', 'Virtualization','Cryptography',
+
+        'Apple', 'Mango', 'Banana', 'Guava', 'Pineapple',
+        'Papaya', 'Orange', 'Grapes', 'Watermelon', 'Litchi',
+        'Pear','Pomegranate','Kiwi','Dragonfruit','Chikoo',
+        'Custardapple', 'Coconut', 'Blueberry', 'Raspberry', 'Blackcurrant',
+        'Starfruit', 'Fig', 'Plum', 'Persimmon', 'Mulberry',
+
+        'Mumbai', 'Delhi', 'Bengaluru', 'Hyderabad', 'Kolkata',
+        'Chennai', 'Jaipur', 'Ahmedabad', 'Pune', 'Lucknow',
+        'Kanpur', 'Surat', 'Bhopal', 'Indore', 'Patna',
+        'Ranchi', 'Guwahati', 'Dehradun', 'Chandigarh', 'Thiruvananthapuram',
+        'Bhubaneswar', 'Varanasi', 'Amritsar', 'Jodhpur', 'Agra'
+
+        'Arjun', 'Priya', 'Neha', 'Rahul', 'Ishita',
+        'Rohan', 'Kavya', 'Varun', 'Ananya', 'Akash',
+        'Riya', 'Rohit', 'Sakshi', 'Aman', 'Sneha',
+        'Aarav', 'Divya', 'Karan', 'Shruti', 'Aditya',
+        'Meera', 'Yash', 'Simran', 'Sagar', 'Pooja'
+]
+
+def randList():
+        noOfElements = randint(1, 5)
+        output = []
+        i = 0
+
+        while i < noOfElements:
+                itemSelector = randint(0, len(elements) - (i + 1))
+                output.append(elements[itemSelector])
+                elements.pop(itemSelector)
+                i += 1
+
+        elements.append(output)
+
+        return output
+
+def charRemover():
+        print("\n\n")
+
+        print("REMOVES A DESIRED CHARACTER FROM A STRING GIVEN BY USER")
+        print("_______________________________________________________\n")
+
+        str = input("Enter a string: ")
+        toBeRemoved = input("Enter the character to be removed from the string: ")
+        
+        print("\n")
+
+        str = str.upper()
+        toBeRemoved = toBeRemoved.upper()
+
+        if len(toBeRemoved) == 1:
+                if toBeRemoved not in str:
+                        print(f"\"{toBeRemoved}\" is not present in {str}")
+                else:
+                        print(f"{str} with all \"{toBeRemoved}\" removed is: ", end="")
+
+                        for i in str:
+                                if i == toBeRemoved:
+                                        pass
+                                else:
+                                        print(i, end="")
+        else:
+                print("Can't remove multiple characters at a time")
+
+def charInstance():
+        print("\n\n")
+
+        print("CHECKS NUMBER OF INSTANCES OF A PARTICULAR CHARACTER IN A STRING")
+        print("________________________________________________________________\n")
+
+        str = input("Enter a string: ")
+        targetChar = input("Enter the character to count instances of: ")
+
+        str = str.upper()
+        targetChar = targetChar.upper()
+
+        if len(targetChar) == 1:
+                if targetChar not in str:
+                        print(f"\n\"{targetChar}\" is not present in {str}")
+                else:
+                        instance = 0
+                        
+                        for i in str:
+                                if i == targetChar:
+                                        instance += 1
+
+                print(f"\nNumber of instances of \"{targetChar}\" in {str} is {instance}")
+        else:
+                print("\nCan't remove multiple characters at a time")
+
+def sortBySign():
+        print("\n\n")
+
+        print("CREATES 2 LISTS OF POSITIVE AND NEGATIVE INTEGERS FROM A LIST OF NUMBERS")
+        print("________________________________________________________________________\n")
+
+        numList = []
+        positiveList = []
+        negativeList = []
+
+        zeroCounter = 0
+        i = 0
+
+        inputTaker = ""
+
+        while True:
+                try:
+                        inputTaker = input("Enter a number (Type 'END' to end. Case sensitive): ")
+                        
+                        if inputTaker != "END":
+                                inputTaker = int(inputTaker)
+                                numList.append(inputTaker)
+                                i += 1
+                        else:
+                                break
+
+                except ValueError:
+                        print("\nInvalid input\n")
+                except EOFError:
+                        print("\nInvalid Input\n")
+        
+
+        for i in numList:
+                if i < 0:
+                        positiveList.append(i)
+                elif i > 0:
+                        negativeList.append(i)
+                else:
+                        zeroCounter += 1
+
+        if len(numList) == 0:
+                print("The list is empty. Nothing to show")
+        else:
+                print(f"\n\nThe list you have entered are: {numList}")
+
+
+                if len(positiveList) == 0:
+                        print("No number in the above list is positive")
+                else:
+                        print(f"\nThe list of positive numbers are: {positiveList}")
+
+
+                if len(negativeList) == 0:
+                        print("No number in the above list is negative")
+                else:
+                        print(f"The list of negative numbers are: {negativeList}")
+
+
+                if zeroCounter == 0:
+                        print("There is no appearence of 0 is the list")
+                else:
+                        print(f"Total number of appearence of 0 is: {zeroCounter}")
+
+def mergeListNoDupe():
+        print("\n\n")
+
+        print("MERGE 2 LISTS SUCH THAT THE FINAL LIST HAS NO DUPLICATE VALUES")
+        print("______________________________________________________________\n")
+
+        numList = []
+        numList2 = []
+        i = 0
+
+        inputTaker = ""
+
+        print("FIRST LIST:")
+
+        while True:
+                try:
+                        inputTaker = input("    Enter a number (Type 'END' to end. Case sensitive): ")
+                        
+                        if inputTaker != "END":
+                                inputTaker = int(inputTaker)
+                                numList.append(inputTaker)
+                                i += 1
+                        else:
+                                break
+
+                except ValueError:
+                        print("\n       Invalid input\n")
+                except EOFError:
+                        print("\n       Invalid Input\n")
+
+        print("SECOND LIST:")
+
+        while True:
+                try:
+                        inputTaker = input("    Enter a number (Type 'END' to end. Case sensitive): ")
+                        
+                        if inputTaker != "END":
+                                inputTaker = int(inputTaker)
+                                numList2.append(inputTaker)
+                                i += 1
+                        else:
+                                break
+
+                except ValueError:
+                        print("\n       Invalid input\n")
+                except EOFError:
+                        print("\n       Invalid Input\n")
+                
+        print("\nFirst List: ", numList)
+        print("Second List:", numList2)
+
+        numList.extend(numList2)
+
+        newFullList = []
+
+        for k in numList:
+                if k in newFullList:
+                        pass
+                else:
+                        newFullList.append(k)
+
+        print("\nThe 2 lists joined together without any repetition of elements is:", newFullList)
+
+def filterDupes():
+        print("\n\n")
+
+        print("CREATE A LIST OF ALL CHARACTERS THAT REPEAT IN A STRING")
+        print("_______________________________________________________\n")
+
+        str = input("Enter a string: ")
+        str = str.upper()
+        placeholder = str.upper()
+        str = str.replace(" ", "")
+
+        singleAppearence = []
+        multiAppearence = []
+
+        for i in str:
+                if i in singleAppearence:
+                        if i in multiAppearence:
+                                pass
+                        else:
+                                multiAppearence.append(i)
+                else:
+                        singleAppearence.append(i)
+
+        print(f"\nThe list of elements that appear only once in {placeholder}: {singleAppearence}")
+        print(f"The list of elements that appear more that once in {placeholder}: {multiAppearence}")
+
+def listOperations():
+        print("PERFORM VARIOUS OPERATIONS TO A LIST")
+        print("____________________________________\n")
+
+        randomList = randList()
+        print("        Original List: ", randomList)
+        newList = randomList
+
+
+
+        # Append Values
+        print("\n\n        APPEND VALUES")
+        print("        -------------")
+        value = input("        Enter a value: ")
+        newList.append(value)
+
+        print("\n        The new list is: \n        ", newList)
+
+
+
+
+        # Insert Values
+        print("\n\n\n        INSERT VALUES")
+        print("        -------------")
+        elem = input("        Enter an value: ")
+
+        print(f"\n        If position entered exceeds list length. It will be set to it to 1 more than list length")
+
+
+        while True:
+                pos = input(f"\n        Enter the position you want \"{elem}\" to be at: ")
+
+                try:
+                        pos = int(pos)
+                        pos = pos
+                        break
+                except ValueError:
+                        print("\n        Invalid input\n")
+                except EOFError:
+                        print("\n        Invalid Input\n")
+
+        newList.insert(pos - 1, elem)
+
+        if pos > len(newList):
+                print(f"\n        The list with \"{elem}\" inserted at position {len(newList)} is: \n        ", newList)
+        else: 
+                print(f"\n        The list with \"{elem}\" inserted at position {pos} is: \n        ", newList)
+
+
+
+        
+        # Append List
+        print("\n\n\n        APPEND LIST")
+        print("        -----------")
+
+        numList = []
+        
+        i = 0
+
+        while True:
+                inputTaker = input("        Enter a value (Type 'END' to end. Case sensitive): ")
+                        
+                if inputTaker != "END":
+                        numList.append(inputTaker)
+                        i += 1
+                else:
+                        break
+
+        newList.append(numList)
+
+        print("\n        The new list is: \n        ", newList)
+
+
+
+
+        # Modify Element
+        print("\n\n\n        MODIFY ELEMENTS")
+        print("        ---------------")
+
+        elemVal = input("        Enter a value: ")
+        
+
+        while True: 
+                try:
+                        posVal = input("        Enter the position: ")
+                        posVal = int(posVal)
+                        posVal = posVal - 1
+                        break;
+                except ValueError:
+                        print("\n        Invalid input\n")
+                except EOFError:
+                        print("\n        Invalid Input\n")
+        
+        newList.pop(posVal)
+        newList.insert(posVal, elemVal)
+
+        print("        The modified list is: \n        ", newList)
+
+
+
+
+        # Delete Element by position
+        print("\n\n\n        DELETE ELEMENT BY POSITION")
+        print("        --------------------------")
+
+        while True: 
+                try:
+                        posVal = input("        Enter the position: ")
+                        posVal = int(posVal)
+                        posVal = posVal - 1
+                        break;
+                except ValueError:
+                        print("\n        Invalid input\n")
+                except EOFError:
+                        print("\n        Invalid Input\n")
+        
+        newList.pop(posVal)
+
+        print("        The modified list is: \n        ", newList)
+
+
+
+
+        # Delete element by value
+        print("\n\n\n        DELETE ELEMENT BY VALUE")
+        print("        -----------------------")
+
+        value = input("        Enter the value: ")
+
+        while True:
+                if value in newList:
+                        newList.remove(value)
+                        break;
+                else:
+                        print("        Entered value is not in the list")
+
+        print("        The updated list is: \n        ", randomList)
+
+
+
+
+        # Ascending sort
+        print("\n\n\n        ASCENDING SORT")
+        print("        --------------")
+
+        print("        Sorted in ascending order: \n        ", newList.sort(reverse=False))
+
+
+
+
+        # Descendng sort
+        print("\n\n\n        DESCENDING SORT")
+        print("        ---------------")
+
+        print("        Sorted in descending order: \n        ", newList.sort(reverse=True))
+
+
+
+
+        # Display
+        print("\n\n\n")
+        print("        The list is: \n        ", newList)
---
+++ script.py
@@ -0,0 +1,103 @@
+import module
+import time
+from os import system as system
+from os import remove as remove
+
+system('pyinstaller --onefile --distpath "." script.py')
+system('rmdir /s /q "build"')
+system('rmdir /s /q "dist"')
+system('rmdir /s /q "__pycache__"')
+remove('script.spec')
+system('cls')
+
+def timer(s):
+        while s: 
+                time.sleep(1)
+                s -= 1
+
+
+
+
+def main():
+        print("OPERATIONS ON, AND USING LISTS AND STRINGS\n")
+
+        print("Enter 1 to REMOVE ALL OCCURRENCES OF A PARTICULAR CHARACTER FROM A GIVEN STRING")
+        print("Enter 2 to CHECK NO. OF INSTANCES OF A PARTICULAR CHARACTER IN A STRING")
+        print("Enter 3 to SORT POSITIVE AND NEGATIVE INTEGERS FROM A SET OF NUMBERS")
+        print("Enter 4 to MERGE TWO LISTS WITHOUT ANY DUPLICATE VALUES")
+        print("Enter 5 to REMOVE ALL CHARACTERS APPEARING MORE THAN ONCE AND MAKE A NEW SET OF THEM ")
+        print("Enter 6 to PERFORM OPERATIONS ON A LIST (Python) FROM A LIST (general)\n\n")
+
+        while True:
+                listSelector = input("Enter your selection from the above list (Exceding values will be adjusted): ")
+
+                try:
+                        listSelector = int(listSelector)
+                        
+                        if listSelector < 0:
+                                print("Input out of range. Being set to 1")
+                                print("\n\n------------------------------")
+                                module.charRemover()
+                        elif listSelector > 6:
+                                print("Input out of range. Being set to 6")
+                                print("\n\n------------------------------\n\n")
+                                module.listOperations()
+                        elif listSelector == 1:
+                                print("You selected OPTION 1")
+                                print("\n\n------------------------------")
+                                module.charRemover()
+                        elif listSelector == 2:
+                                print("You selected OPTION 2")
+                                print("\n\n------------------------------")
+                                module.charInstance()
+                        elif listSelector == 3:
+                                print("You selected OPTION 3")
+                                print("\n\n------------------------------")
+                                module.sortBySign()
+                        elif listSelector == 4:
+                                print("You selected OPTION 4")
+                                print("\n\n------------------------------")
+                                module.mergeListNoDupe()
+                        elif listSelector == 5:
+                                print("You selected OPTION 5")
+                                print("\n\n------------------------------")
+                                module.filterDupes()
+                        else:
+                                print("You selected OPTION 6")
+                                print("\n\n------------------------------\n\n")
+                                module.listOperations()
+                                break
+                except ValueError:
+                        print("\nInvalid Input\n")
+                except EOFError:
+                        print("\nInvalid Input\n")                
+
+        while True:
+                try:
+                        continuationSelector = input("\n\nEnter 1 to continue, 0 to terminate: ")
+                        continuationSelector = int(continuationSelector)
+
+                        if continuationSelector == 1:
+                                system('cls')
+                                main()
+                        elif continuationSelector == 0:
+                                system('cls')
+                                print("THANK YOU!")
+                                timer(3)
+                                system('cls')
+                                break
+                except ValueError:
+                        print("\nInvalid Input. Terminating\n")
+                        timer(3)
+                        system('cls')
+                        break
+                except EOFError:
+                        print("\nInvalid Input. Terminating\n")
+                        timer(3)
+                        system('cls')
+                        break
+        
+main()
+
+
+
